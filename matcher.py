becf5d91cf7a7ec26a396d4a5f548467e8decc55
Peter Elmers <peter.elmers@yahoo.com>
2017-03-02 14:22:19 -0600

    Document matcher interface.

--- matcher.py
+++ matcher.py
@@ -1,22 +1,24 @@
-from os.path import basename, splitext
-
-def loc_from_filename(path):
-    """Return x,y,o from filename.
-    """
-    f = basename(path)
-    root, _ = splitext(f)
-    x, y, o = root.split('_')
-    return float(x), float(y), o
-
 class ImageMatcher(object):
+    """Interface for image matching algorithms.
+    """
 
     def train(self, train_paths):
+        """Train the matcher object by from given training data.
+        """
         pass
 
     def match_test_image(self, q_path):
+        """Match query image at q_path against the model.
+        Return list of results,
+        where each result is pair (training image path, score)
+        """
         pass
 
     def debug_display(self, q_path, matches):
+        """Optional method. Display any helpful debugging information for the
+        given query image that returned the provided matches. matches is the
+        output of some previous call to match_test_image.
+        """
         pass
 
     def __eq__(self, other):
